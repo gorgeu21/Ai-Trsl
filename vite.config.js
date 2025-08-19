185219a541450a1bd91afc37c609496d4f32e7ee
gorgeu21 <gorgeu21@gmail.com>
2025-08-19 22:14:41 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // замените <repo> на имя вашего репозитория; если деплоите на username.github.io, можно оставить '/'
-  plugins: [react()]
-})
